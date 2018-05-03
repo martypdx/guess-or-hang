ab2a78d3474a489e826750bad64f1ad62c93b2e6
martypdx <marty@martypdx.com>
2018-05-02 19:27:48 -0700

    inital game

---
+++ app.js
@@ -0,0 +1,87 @@
+/* exported guess */
+
+const words = [
+    'orange',
+    'apple',
+    'cherries'
+];
+
+var word = '';
+var guessed = '';
+var guessCount = 0;
+var incorrectCount = 0;
+
+// from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function loadWord() {
+    var randomIndex = getRandomIndex(words.length);
+    word = words[randomIndex];
+
+    var spans = document.querySelectorAll('.word-letters span');
+
+    for(var i = 0; i < word.length; i++) {
+        spans[i].textContent = word[i];
+        spans[i].style.visibility = 'visible';
+        spans[i].style.color = 'white';
+    }
+
+    for(var j = word.length; j < spans.length; j++) {
+        spans[j].style.visibility = 'hidden';
+    }
+
+    guessed = '';
+}
+
+loadWord();
+
+function guess() {
+    var guessInput = document.getElementById('guess');
+    var letter = guessInput.value.trim().toLowerCase();
+
+    if(letter === '') {
+        alert('No letter guessed!');
+    }
+    else if(guessed.includes(letter)) {
+        alert('Already guessed ' + letter);
+    }
+    else {
+        guessed += letter;
+        guessCount++;
+        document.getElementById('guess-count').textContent = guessCount;
+        document.getElementById('guessed').textContent += ' ' + letter;
+        guessInput.value = '';
+
+        if(word.includes(letter)) {
+            
+            for(var i = 0; i < word.length; i++) {
+                if(word[i] === letter) {
+                    document.getElementById('letter-' + i).style.color = 'black';
+                }
+            }
+            
+            var win = true;
+            for(var j = 0; j < word.length; j++) {
+                if(!guessed.includes(word[j])) {
+                    win = false;
+                }
+            }
+
+            // check for win!
+            if(win) {
+                alert('you win!');
+                document.getElementById('guess-btn').disabled = true;
+            }
+        }
+        else {
+            incorrectCount++;
+            document.getElementById('part-' + incorrectCount).style.visibility = 'visible';
+            if(incorrectCount >= 6) {
+                alert('you lose (and die along the way)!');
+                document.getElementById('guess-btn').disabled = true;
+            }
+        }
+    }
+}
